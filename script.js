aec976407b51c3d4df27a527106ce110fc40d16e
Teo Ma <teo_matt@msn.com>
2023-03-27 13:49:48 +0800

    update new style

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // 在此处添加JavaScript代码  
+// This script sets the href of all links to a domain that is different from the current domain
+var links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+	if (links[i].hostname != window.location.hostname) {
+		links[i].target = '_blank';
+		links[i].rel = 'noopener noreferrer';
+		links[i].href = 'https://example.com/';
+	}
+}
